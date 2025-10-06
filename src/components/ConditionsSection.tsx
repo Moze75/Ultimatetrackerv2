a7536234e3ac9b2ed22d822dccb4e679833b0a8a
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-10-06 22:36:01 +0200

    Updated ConditionsSection.tsx

--- src/components/ConditionsSection.tsx
+++ src/components/ConditionsSection.tsx
@@ -6,146 +6,156 @@ import toast from 'react-hot-toast';
 
 export const CONDITIONS: Condition[] = [
   {
-    id: 'blinded',
-    name: 'Aveuglé',
-    description: 'Une créature aveuglée ne peut pas voir.',
+    id: 'prone',
+    name: 'À terre',
+    description: 'Tant que vous avez l\'état À terre, vous subissez les effets suivants.',
     effects: [
-      'La créature rate automatiquement tout test de caractéristique qui nécessite la vue',
-      'Les jets d\'attaque contre la créature ont l\'avantage',
-      'Les jets d\'attaque de la créature ont le désavantage'
-    ]
-  },
-  {
-    id: 'charmed',
-    name: 'Charmé',
-    description: 'Une créature charmée ne peut pas attaquer celui qui l\'a charmée.',
-    effects: [
-      'La créature ne peut pas attaquer celui qui l\'a charmée',
-      'Celui qui a charmé la créature a l\'avantage aux tests de Charisme pour interagir avec elle'
-    ]
-  },
-  {
-    id: 'deafened',
-    name: 'Assourdi',
-    description: 'Une créature assourdie ne peut pas entendre.',
-    effects: [
-      'La créature rate automatiquement tout test de caractéristique qui nécessite l\'ouïe'
-    ]
-  },
-  {
-    id: 'frightened',
-    name: 'Effrayé',
-    description: 'Une créature effrayée a le désavantage aux tests tant que la source de sa peur est visible.',
-    effects: [
-      'La créature a le désavantage aux tests de caractéristique tant que la source de sa peur est visible',
-      'La créature ne peut pas se rapprocher volontairement de la source de sa peur'
+      'Déplacement limité : Vos seules possibilités de déplacement sont ramper ou vous relever en dépensant la moitié de votre Vitesse (arrondie à l\'inférieur), ce qui met un terme à l\'état. Si votre Vitesse est de 0, vous ne pouvez pas vous relever',
+      'Effet sur les attaques : Vous subissez le Désavantage aux jets d\'attaque. Un jet d\'attaque contre vous reçoit l\'Avantage si l\'assaillant se trouve dans un rayon de 1,50 m de vous. Sans cela, ce jet d\'attaque subit le Désavantage'
     ]
   },
   {
     id: 'grappled',
     name: 'Agrippé',
-    description: 'Une créature agrippée ne peut pas se déplacer.',
+    description: 'Tant que vous avez l\'état Agrippé, vous subissez les effets suivants.',
     effects: [
-      'La vitesse de la créature devient 0',
-      'L\'état prend fin si celui qui agrippe est incapable d\'agir'
+      'Vitesse 0 : Votre Vitesse est de 0 et ne peut pas augmenter',
+      'Effet sur les attaques : Vous subissez le Désavantage aux jets d\'attaque contre toute cible hormis l\'agrippeur',
+      'Déplaçable : L\'agrippeur peut vous tirer ou vous porter lorsqu\'il se déplace, mais ses coûts de déplacement sont doublés, sauf si vous êtes de taille TP ou que votre catégorie est inférieure d\'au moins deux crans à la sienne'
     ]
   },
   {
-    id: 'incapacitated',
-    name: 'Incapable d\'agir',
-    description: 'Une créature incapable d\'agir ne peut effectuer aucune action ni réaction.',
+    id: 'deafened',
+    name: 'Assourdi',
+    description: 'Tant que vous avez l\'état Assourdi, vous subissez les effets suivants.',
     effects: [
-      'La créature ne peut effectuer aucune action ni réaction'
+      'Incapable d\'entendre : Vous n\'entendez rien, et ratez automatiquement tous les tests de caractéristique qui reposent sur l\'ouïe'
     ]
   },
   {
-    id: 'invisible',
-    name: 'Invisible',
-    description: 'Une créature invisible ne peut pas être vue.',
+    id: 'blinded',
+    name: 'Aveuglé',
+    description: 'Tant que vous avez l\'état Aveuglé, vous subissez les effets suivants.',
     effects: [
-      'La créature est impossible à voir sans magie ou sens spécial',
-      'La créature est considérée comme fortement obscurcie',
-      'La position de la créature peut être détectée par le bruit ou les traces',
-      'Les jets d\'attaque contre la créature ont le désavantage',
-      'Les jets d\'attaque de la créature ont l\'avantage'
+      'Incapable de voir : Vous ne voyez rien et ratez automatiquement tout test de caractéristique reposant sur la vue',
+      'Effet sur les attaques : Les jets d\'attaque contre vous ont l\'Avantage, et vos jets d\'attaque subissent le Désavantage'
     ]
   },
   {
-    id: 'paralyzed',
-    name: 'Paralysé',
-    description: 'Une créature paralysée est incapable d\'agir et de parler.',
+    id: 'charmed',
+    name: 'Charmé',
+    description: 'Tant que vous avez l\'état Charmé, vous subissez les effets suivants.',
     effects: [
-      'La créature est incapable d\'agir et de parler',
-      'La créature rate automatiquement les jets de sauvegarde de Force et de Dextérité',
-      'Les jets d\'attaque contre la créature ont l\'avantage',
-      'Toute attaque qui touche la créature à moins de 1,50 mètre est un coup critique'
+      'Ne pas nuire au charmeur : Vous ne pouvez pas attaquer votre « charmeur » ni le cibler avec des aptitudes ou effets magiques qui infligent des dégâts',
+      'Interaction avec Avantage : Le charmeur a l\'Avantage aux tests de caractéristique d\'interaction sociale avec vous'
     ]
   },
   {
-    id: 'petrified',
-    name: 'Pétrifié',
-    description: 'Une créature pétrifiée est transformée en substance solide inanimée.',
+    id: 'frightened',
+    name: 'Effrayé',
+    description: 'Tant que vous avez l\'état Effrayé, vous subissez les effets suivants.',
     effects: [
-      'La créature est transformée en substance solide inanimée',
-      'Son poids est multiplié par dix',
-      'La créature ne vieillit plus',
-      'La créature est incapable d\'agir',
-      'Les jets d\'attaque contre la créature ont l\'avantage',
-      'La créature rate automatiquement les jets de sauvegarde de Force et de Dextérité',
-      'La créature est résistante à tous les dégâts'
+      'Effet sur les attaques et les tests de caractéristique : Vous subissez le Désavantage aux tests de caractéristique et aux jets d\'attaque tant que la source de votre effroi est dans votre champ de vision',
+      'Impossible d\'approcher : Vous ne pouvez pas vous rapprocher volontairement de la source de votre effroi'
     ]
   },
   {
     id: 'poisoned',
     name: 'Empoisonné',
-    description: 'Une créature empoisonnée subit un désavantage aux jets d\'attaque et aux tests de caractéristique.',
+    description: 'Tant que vous avez l\'état Empoisonné, vous subissez l\'effet suivant.',
     effects: [
-      'La créature a le désavantage aux jets d\'attaque',
-      'La créature a le désavantage aux tests de caractéristique'
-    ]
-  },
-  {
-    id: 'prone',
-    name: 'À terre',
-    description: 'Une créature à terre a une mobilité limitée.',
-    effects: [
-      'La seule option de mouvement est de ramper',
-      'La créature a le désavantage aux jets d\'attaque',
-      'Les jets d\'attaque au corps à corps contre la créature ont l\'avantage',
-      'Les jets d\'attaque à distance contre la créature ont le désavantage'
+      'Effet sur les attaques et les tests de caractéristique : Vous subissez le Désavantage aux jets d\'attaque et aux tests de caractéristique'
     ]
   },
   {
     id: 'restrained',
     name: 'Entravé',
-    description: 'Une créature entravée voit sa vitesse réduite à 0.',
+    description: 'Tant que vous avez l\'état Entravé, vous subissez les effets suivants.',
     effects: [
-      'La vitesse de la créature devient 0',
-      'Les jets d\'attaque contre la créature ont l\'avantage',
-      'Les jets d\'attaque de la créature ont le désavantage',
-      'La créature a le désavantage aux jets de sauvegarde de Dextérité'
+      'Vitesse 0 : Votre Vitesse est de 0 et ne peut pas augmenter',
+      'Effet sur les attaques : Les jets d\'attaque contre vous ont l\'Avantage, et vos jets d\'attaque subissent le Désavantage',
+      'Effet sur les jets de sauvegarde : Vous subissez le Désavantage aux jets de sauvegarde de Dextérité'
+    ]
+  },
+  {
+    id: 'exhaustion',
+    name: 'Épuisement',
+    description: 'Tant que vous avez l\'état Épuisement, vous subissez les effets suivants.',
+    effects: [
+      'Niveaux d\'Épuisement : Cet état est cumulatif. Chaque fois que vous le recevez, vous subissez 1 niveau d\'Épuisement. Vous mourez quand votre niveau d\'Épuisement atteint 6',
+      'Tests d20 affectés : Lorsque vous effectuez un Test d20, le résultat est réduit de 2 fois votre niveau actuel d\'Épuisement',
+      'Vitesse réduite : Votre Vitesse est réduite de 1,50 m x votre niveau actuel d\'Épuisement',
+      'Suppression des niveaux d\'Épuisement : Terminer un Repos long dissipe 1 niveau d\'Épuisement. Lorsque votre niveau d\'Épuisement atteint 0, l\'état prend fin pour vous'
     ]
   },
   {
     id: 'stunned',
     name: 'Étourdi',
-    description: 'Une créature étourdie est incapable d\'agir et parle de manière hésitante.',
+    description: 'Tant que vous avez l\'état Étourdi, vous subissez les effets suivants.',
     effects: [
-      'La créature est incapable d\'agir et parle de manière hésitante',
-      'La créature rate automatiquement les jets de sauvegarde de Force et de Dextérité',
-      'Les jets d\'attaque contre la créature ont l\'avantage'
+      'Neutralisé : Vous subissez l\'état Neutralisé',
+      'Effet sur les jets de sauvegarde : Vous ratez automatiquement vos jets de sauvegarde de Force et de Dextérité',
+      'Effet sur les attaques : Les jets d\'attaque contre vous ont l\'Avantage'
     ]
   },
   {
     id: 'unconscious',
     name: 'Inconscient',
-    description: 'Une créature inconsciente est incapable d\'agir et n\'est pas consciente de son environnement.',
+    description: 'Tant que vous avez l\'état Inconscient, vous subissez les effets suivants.',
     effects: [
-      'La créature est incapable d\'agir et n\'est pas consciente de son environnement',
-      'La créature lâche ce qu\'elle tient et tombe à terre',
-      'La créature rate automatiquement les jets de sauvegarde de Force et de Dextérité',
-      'Les jets d\'attaque contre la créature ont l\'avantage',
-      'Toute attaque qui touche la créature à moins de 1,50 mètre est un coup critique'
+      'Inerte : Vous subissez les états À terre et Neutralisé, et laissez choir tout ce que vous teniez. Lorsque cet état prend fin, vous êtes toujours À terre',
+      'Vitesse 0 : Votre Vitesse est de 0 et ne peut pas augmenter',
+      'Effet sur les attaques : Les jets d\'attaque contre vous ont l\'Avantage',
+      'Effet sur les jets de sauvegarde : Vous ratez automatiquement vos jets de sauvegarde de Force et de Dextérité',
+      'Coups critiques automatiques : Tout jet d\'attaque qui vous touche est un Coup critique si l\'assaillant qui la porte se trouve dans un rayon de 1,50 m',
+      'Dénué de conscience : Vous n\'avez pas conscience de ce qui vous entoure'
+    ]
+  },
+  {
+    id: 'invisible',
+    name: 'Invisible',
+    description: 'Tant que vous avez l\'état Invisible, vous subissez les effets suivants.',
+    effects: [
+      'Surprise : Si vous êtes Invisible au moment de jouer l\'Initiative, vous avez l\'Avantage à ce jet',
+      'Dissimulé : Vous n\'êtes pas affecté par les effets qui exigent que la cible soit vue, sauf si le créateur de l\'effet vous « voit » par un biais ou un autre. Tout l\'équipement que vous portez est lui aussi dissimulé',
+      'Effet sur les attaques : Les jets d\'attaque contre vous subissent le Désavantage, et vos jets d\'attaque ont l\'Avantage. Si une créature vous voit par un biais ou un autre, vous ne recevez pas ce bénéfice contre elle'
+    ]
+  },
+  {
+    id: 'incapacitated',
+    name: 'Neutralisé',
+    description: 'Tant que vous avez l\'état Neutralisé, vous subissez les effets suivants.',
+    effects: [
+      'Inactif : Vous ne pouvez entreprendre ni action, ni action Bonus ni Réaction',
+      'Concentration brisée : Votre Concentration est brisée',
+      'Sans voix : Vous ne pouvez pas parler',
+      'Surpris : Si vous êtes Neutralisé au moment de jouer l\'Initiative, vous subissez le Désavantage à ce jet'
+    ]
+  },
+  {
+    id: 'paralyzed',
+    name: 'Paralysé',
+    description: 'Tant que vous avez l\'état Paralysé, vous subissez les effets suivants.',
+    effects: [
+      'Neutralisé : Vous subissez l\'état Neutralisé',
+      'Vitesse 0 : Votre Vitesse est de 0 et ne peut pas augmenter',
+      'Effet sur les jets de sauvegarde : Vous ratez automatiquement vos jets de sauvegarde de Force et de Dextérité',
+      'Effet sur les attaques : Les jets d\'attaque contre vous ont l\'Avantage',
+      'Coups critiques automatiques : Tout jet d\'attaque qui vous touche est un Coup critique si l\'assaillant qui la porte se trouve dans un rayon de 1,50 m'
+    ]
+  },
+  {
+    id: 'petrified',
+    name: 'Pétrifié',
+    description: 'Tant que vous avez l\'état Pétrifié, vous subissez les effets suivants.',
+    effects: [
+      'Transformé en substance inanimée : Vous êtes transformé, ainsi que les objets non magiques que vous portez, en une substance inanimée et dense (généralement de la pierre). Votre poids est décuplé et vous n\'êtes plus soumis au vieillissement',
+      'Neutralisé : Vous subissez l\'état Neutralisé',
+      'Vitesse 0 : Votre Vitesse est de 0 et ne peut pas augmenter',
+      'Effet sur les attaques : Les jets d\'attaque contre vous ont l\'Avantage',
+      'Effet sur les jets de sauvegarde : Vous ratez automatiquement vos jets de sauvegarde de Force et de Dextérité',
+      'Résistance aux dégâts : Vous avez la Résistance à tous les dégâts',
+      'Immunité contre le poison : Vous avez l\'Immunité contre l\'état Empoisonné'
     ]
   }
 ];
@@ -159,7 +169,6 @@ export function ConditionsSection({ player, onUpdate }: ConditionsSectionProps) 
   const [selectedCondition, setSelectedCondition] = useState<Condition | null>(null);
   const [conditionsExpanded, setConditionsExpanded] = useState(false);
 
-  // Early return if player is undefined
   if (!player) {
     return (
       <div className="stat-card">
@@ -217,14 +226,11 @@ export function ConditionsSection({ player, onUpdate }: ConditionsSectionProps) 
         active_conditions: newConditions
       });
 
-      const condition = CONDITIONS.find(c => c.id === conditionId);
-      if (condition) {
-        toast.success(
-          activeConditions.includes(conditionId)
-            ? `État retiré : ${condition.name}`
-            : `État ajouté : ${condition.name}`
-        );
-      }
+      toast.success(
+        activeConditions.includes(conditionId)
+          ? `État retiré : ${condition.name}`
+          : `État ajouté : ${condition.name}`
+      );
 
       updateSelectedCondition(condition, newConditions.includes(conditionId));
     } catch (error) {
@@ -286,7 +292,7 @@ export function ConditionsSection({ player, onUpdate }: ConditionsSectionProps) 
               <div className="space-y-2">
                 {selectedCondition.effects.map((effect, index) => (
                   <div key={index} className="flex items-start gap-2 text-sm">
-                    <div className="w-2 h-2 mt-1.5 rounded-full bg-orange-500/50" />
+                    <div className="w-2 h-2 mt-1.5 rounded-full bg-orange-500/50 flex-shrink-0" />
                     <p className="text-gray-400 flex-1">{effect}</p>
                   </div>
                 ))}
