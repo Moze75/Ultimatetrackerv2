818d4492f17cdefafffb20510f5aa98cf8bf297b
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-10-21 16:37:13 +0200

    Updated PlayerProfile.tsx

--- src/components/PlayerProfile.tsx
+++ src/components/PlayerProfile.tsx
@@ -610,6 +610,13 @@ const totalAC = armorAC + shieldBonus + acBonus;
         onUpdate={onUpdate}
       />
 
+      {/* ✅ NOUVEAU : Modal Campagnes */}
+      <CampaignPlayerModal
+        open={showCampaignModal}
+        onClose={() => setShowCampaignModal(false)}
+        player={player}
+        onUpdate={onUpdate}
+      />
     </>
   );
 }
