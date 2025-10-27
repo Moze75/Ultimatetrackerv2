06a538e8fb6ea34a1a9383fa06999e1f4f18c0cd
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-10-27 21:37:16 +0100

    Updated CombatTab.tsx

--- src/components/CombatTab.tsx
+++ src/components/CombatTab.tsx
@@ -7,7 +7,6 @@ import { DiceRoller } from '../components/DiceRoller';
 import { StandardActionsSection } from './StandardActionsSection';
 import { attackService } from '../services/attackService';
 import { supabase } from '../lib/supabase';
-import { inferWeaponAbilityMod } from '../utils/inferWeaponAbility';
 import './combat-tab.css';
 
 interface CombatTabProps {
