5411ad5072373ea1ee66d8d827c47fbe53ef03bf
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-10-27 23:14:12 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -352,7 +352,14 @@ useEffect(() => {
     );
   }
 
-
+<Toaster
+  position="top-right"
+  containerStyle={{ zIndex: 20000 }} // s’assure d’être au-dessus de la modale
+  toastOptions={{
+    // Optionnel: styles globaux
+    style: { zIndex: 20000 },
+  }}
+/>
   
   // Rendu principal
   return (
