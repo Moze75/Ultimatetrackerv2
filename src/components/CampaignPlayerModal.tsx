ebe628ec752a0c9d697b4e616e4b9375e527c3ef
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-10-28 14:56:29 +0100

    Updated CampaignPlayerModal.tsx

--- src/components/CampaignPlayerModal.tsx
+++ src/components/CampaignPlayerModal.tsx
@@ -1236,7 +1236,7 @@ return (
                           {pendingGifts.length} loot{pendingGifts.length > 1 ? 's' : ''} en attente !
                         </h3>
                         <p className="text-sm text-gray-300">
-                          Vous avez des objets à récupérer {pendingGifts.length > 1 ? 'des objets' : 'un objet'} pour votre aventure.
+                           {pendingGifts.length > 1 ? 'des objets' : 'un objet'} 
                           Récupérez-{pendingGifts.length > 1 ? 'les' : 'le'} ci-dessous.
                         </p>
                       </div>
