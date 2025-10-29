c752da10605b574e684ab779fbb434da4f56032a
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-10-29 22:49:26 +0100

    Updated GamePage.tsx

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -21,6 +21,9 @@ import { inventoryService } from '../services/inventoryService';
 import PlayerProfileProfileTab from '../components/PlayerProfileProfileTab';
 import { loadAbilitySections } from '../services/classesContent';
 
+import { HPManager } from '../components/HPManager';
+import { InventoryManager } from '../components/InventoryManager';
+
 
 import { PlayerProfileSettingsModal } from '../components/PlayerProfileSettingsModal';  
 
