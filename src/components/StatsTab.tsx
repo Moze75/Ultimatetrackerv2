6daa39084f27ee4a19bed137f0281c677a6540fc
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-10-29 15:46:52 +0100

    Updated StatsTab.tsx

--- src/components/StatsTab.tsx
+++ src/components/StatsTab.tsx
@@ -342,7 +342,7 @@ export function StatsTab({ player, onUpdate }: StatsTabProps) {
                   </div>
 
                   {/* Valeur de la caractéristique (bulle invisible) */}
-                  <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2">
+                  <div className="absolute bottom-4 left-[45%] transform -translate-x-1/2">
                     {editing ? (
                       <input
                         type="number"
